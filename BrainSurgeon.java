e1f1d1ec9c417efdf24d3ff7dbcb0a54c05bb3a4
ibrahim070 <41923816+ibrahim070@users.noreply.github.com>
2018-10-24 08:54:55 +0400

    Add files via upload

---
+++ BrainSurgeon.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class BrainSurgeon extends Surgeon{
+    public BrainSurgeon(String name, String surname, int age) {
+        super(name, surname, age);
+    }
+}
---
+++ Surgeon.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Surgeon extends Doctors{
+    public Surgeon(String name, String surname, int age) {
+        super(name, surname, age);
+    }
+    public void operate (){
+
+    }
+}
